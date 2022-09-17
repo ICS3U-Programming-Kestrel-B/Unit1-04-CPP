c714755d5e1020c43bf658ddf1c56ff8e72111b6
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2022-09-17 21:36:17 +0000

    “finished”

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 KestrelBryce All rights reserved
+//
+// Created by KestrelBryce
+// Date: Sep. 17, 2022
+// This program calculates the area and perimeter of a rectangle with 9cm x 4cm dimensions
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions:\n";
+    std::cout << "9cm x 4cm\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 9 * 4 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (9 + 4) << " cm\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 KestrelBryce All rights reserved
+//
+// Created by KestrelBryce
+// Date: Sep. 17, 2022
+// This program does basic math.
+
+#include <iostream>
+
+int main() {
+    // Division
+    std::cout << "5/2 = " << (5 / 2) << "\n";
+    // Power
+    std::cout << "5**2 = " << (5 * 5) << "\n";
+    // Square root
+    std::cout << "√25 = " << (5) << "\n";
+}
